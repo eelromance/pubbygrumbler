1bbb99503668dc19a0f72c3129145efbe8792e5c
Cameron Lee <cameronlee@Camerons-MacBook-Pro.local>
2018-09-04 15:21:44 -0500

    initial commit

---
+++ js/grumbler.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble1.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub1 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble2.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub2 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble3.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub3 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble4.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub4 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble5.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub5 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble6.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub6 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble7.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub7 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble8.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub8 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble9.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub9 path").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/wav/grumble10.wav";
+    obj.volume=0.10;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#pub10 path").click(function() {
+        obj.play();
+    });
+});
